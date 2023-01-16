d54912d323895818d81b9b754418ba19d3f96fbb
FelipeBrocca <pipebrocca@gmail.com>
2023-01-16 15:17:19 -0300

    EventsAsk

--- src/Components/EventsAsk/EventsAsk.js
+++ src/Components/EventsAsk/EventsAsk.js
@@ -1,32 +1,33 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './EventsAsk.css'
 
 const EventsAsk = () => {
-  return (
-    <section className="section-events" id="events">
-        <div className="section-container">
-            <h2 className="section-title">
-                Cotizá tu Evento
-            </h2>
-            <p className="section-events-txt">
-                Querés conocer mas acerca de nuestros eventos? <br/> Completá tus datos en Whatsapp y enterate
-            </p>
-            <a href="https://wa.me/5491135781101?text=Hola!%20Soy%20%20%20%20%20%20y%20me%20interesaría%20cotizar%20un%20evento%20para%20%20%20%20personas%20el%20dia%20%20%20%20.Muchas%20Gracias!%20Espero%20tu%20respuesta%20" 
-            target="_blank" rel="noreferrer">
-            <button className="button-events">
-                <div className="svg-wrapper-1">
-                    <div className="svg-wrapper">
-                        <svg viewBox="0 0 32 32" width="24px" heigth="24px" className="whatsapp-ico">
-                            <path d=" M19.11 17.205c-.372 0-1.088 1.39-1.518 1.39a.63.63 0 0 1-.315-.1c-.802-.402-1.504-.817-2.163-1.447-.545-.516-1.146-1.29-1.46-1.963a.426.426 0 0 1-.073-.215c0-.33.99-.945.99-1.49 0-.143-.73-2.09-.832-2.335-.143-.372-.214-.487-.6-.487-.187 0-.36-.043-.53-.043-.302 0-.53.115-.746.315-.688.645-1.032 1.318-1.06 2.264v.114c-.015.99.472 1.977 1.017 2.78 1.23 1.82 2.506 3.41 4.554 4.34.616.287 2.035.888 2.722.888.817 0 2.15-.515 2.478-1.318.13-.33.244-.73.244-1.088 0-.058 0-.144-.03-.215-.1-.172-2.434-1.39-2.678-1.39zm-2.908 7.593c-1.747 0-3.48-.53-4.942-1.49L7.793 24.41l1.132-3.337a8.955 8.955 0 0 1-1.72-5.272c0-4.955 4.04-8.995 8.997-8.995S25.2 10.845 25.2 15.8c0 4.958-4.04 8.998-8.998 8.998zm0-19.798c-5.96 0-10.8 4.842-10.8 10.8 0 1.964.53 3.898 1.546 5.574L5 27.176l5.974-1.92a10.807 10.807 0 0 0 16.03-9.455c0-5.958-4.842-10.8-10.802-10.8z" fillRule="evenodd">                 </path>
-                        </svg>
-                    </div>
-                </div>
-                <span>¡Escribinos!</span>
-            </button>
-            </a>    
-        </div>
-    </section>
-  )
+    return (
+        <section className="section-events" id="events">
+            <div className="section-container">
+                <h2 className="section-title">
+                    Cotizá tu Evento
+                </h2>
+                <p className="section-events-txt">
+                    Querés conocer mas acerca de nuestros eventos? <br /> Completá tus datos y enterate
+                </p>
+                <Link>
+                    <button className="button-events">
+                        <div className="svg-wrapper-1">
+                            <div className="svg-wrapper">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" fill="currentColor" class="whatsapp-ico" width="24px" height="24px">
+                                    <path d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32l8.4-8.4z" />
+                                    <path d="M5.25 5.25a3 3 0 00-3 3v10.5a3 3 0 003 3h10.5a3 3 0 003-3V13.5a.75.75 0 00-1.5 0v5.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5V8.25a1.5 1.5 0 011.5-1.5h5.25a.75.75 0 000-1.5H5.25z" />
+                                </svg>
+                            </div>
+                        </div>
+                        <span>¡Consultános!</span>
+                    </button>
+                </Link>
+            </div>
+        </section>
+    )
 }
 
 export default EventsAsk
