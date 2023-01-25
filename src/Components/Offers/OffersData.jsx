1bf3142c7592c13a349f08e32926294c7a46fe6e
FelipeBrocca <pipebrocca@gmail.com>
2023-01-25 18:40:28 -0300

    changes in cart

--- src/Components/Offers/OffersData.jsx
+++ src/Components/Offers/OffersData.jsx
@@ -3,99 +3,42 @@ export const offersData = [
       id: 1000,
       title: '5-4-1',
       subtitle: '5 MUZZA + 4 A ELECCIÓN + 1 POLLO',
-      offer: [{
-        name: 'Muzza.',
-        quantity: 5
-      },{
-        name: 'a elección',
-        quantity: 4,
-      },{
-        name: 'Pollo',
-        quantity: 1
-      }],
+      offer: '5 Muzza., 4 a elección, 1 Pollo',
       price: 7500
     },
     { 
       id: 1001,
       title: '4-4-3',
-      subtitle: '4 MUZZA + 3 CEBOLLA + 3 A ELECCIÓN',
-      offer: [{
-        name: 'Muzza.',
-        quantity: 4
-      },{
-        name: 'a elección',
-        quantity: 3,
-      },{
-        name: 'Cebolla',
-        quantity: 3
-      }],
+      subtitle: '4 MUZZA + 4 CEBOLLA + 3 A ELECCIÓN',
+      offer: '4 Muzza., 4 Cebolla, 3 a elección',
       price: 7600
     },
     { 
       id: 1002,
       title: '4-3-1-2',
-      subtitle: '4 MUZZA + 4 A ELECCIÓN + 1 ROQUEFORT + 2 PROVOLONE',
-      offer: [{
-        name: 'Muzza.',
-        quantity: 4
-      },{
-        name: 'a elección',
-        quantity: 4,
-      },{
-        name: 'Roque.',
-        quantity: 1
-      },{
-        name: 'Provo.',
-        quantity: 2
-      }],
+      subtitle: '4 MUZZA + 3 A ELECCIÓN + 1 ROQUEFORT + 2 PROVOLONE',
+      offer: '4 Muzza., 3 a elección, 1 Roque., 2 Provo.',
       price: 7900
     },
     {
         id: 1003,
         title: '3-4-3',
         subtitle: '3 MUZZA + 4 A ELECCIÓN + 3 CUATRO QUESOS',
-        offer: [{
-          name: 'Muzza.',
-          quantity: 3
-        },{
-          name: 'a elección',
-          quantity: 4,
-        },{
-          name: 'Cuatro Quesos',
-          quantity: 3
-        }],
+        offer: '3 Muzza., 4 a elección, 3 Cuatro quesos',
         price: 8200
       },
       { 
         id: 1004,
         title: '3-5-2',
-        subtitle: '4 MUZZA + 5 A ELECCIÓN + 2 JAMÓN',
-        offer: [{
-          name: 'Muzza.',
-          quantity: 4
-        },{
-          name: 'a elección',
-          quantity: 5,
-        },{
-          name: 'Jamón',
-          quantity: 2
-        }],
+        subtitle: '3 MUZZA + 5 A ELECCIÓN + 2 JAMÓN',
+        offer: '3 Muzza., 5 a elección, 2 Jamón',
         price: 7900
       },
       { 
         id: 1005,
         title: '4-4-2',
         subtitle: '4 MUZZA + 4 A ELECCIÓN + 2 POLLO',
-        offer: [{
-          name: 'Muzza.',
-          quantity: 4
-        },{
-          name: 'a elección',
-          quantity: 4,
-        },{
-          name: 'Pollo',
-          quantity: 2
-        }],
+        offer: '4 Muzza., 4 a elección, 2 Pollo',
         price: 7900
       }
 ]
