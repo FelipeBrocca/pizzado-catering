577461c84795f821d87d6287fb5f3b5c584ab351
jmacera95 <74208045+jmacera95@users.noreply.github.com>
2024-02-09 16:10:52 -0300

    Improve events images slider. Fix bug related to css properties

--- src/Components/EventsForm/EventInfo.jsx
+++ src/Components/EventsForm/EventInfo.jsx
@@ -1,85 +1,67 @@
-import React, { lazy, Suspense } from 'react'
+import React, { lazy, Suspense } from 'react';
+import Skeleton from 'react-loading-skeleton';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 // import required modules
-import { Autoplay, Pagination } from 'swiper/modules';
+import { Autoplay } from 'swiper/modules';
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
 import { DataEvents } from './DataEvents';
 
-const LazyInfoImages = lazy(() => import('./LazyInfoImages'))
+const ImageLazy = lazy(() => import('../ImageLazy/ImageLazy'));
 
 const EventInfo = () => {
-    return (
-        <>
-            <h2 className="titulo-register">COTIZÁ TU EVENTO</h2>
-            <div className='top-form'>
-                <p>
-                    Más de 500 eventos realizados: Corporativos, fiestas, cumpleanos y casamientos.
-                    <br></br>El servicio incluye a los cocineros y mozos. 
-                    <br></br>Trabajamos en la parrilla u horno que haya en el lugar, aunque también tenemos nuestras parrillas movibles en caso de ser necesarias.(*)
-                </p>
-                {/* <div className='form-imges-container'>
-                    <div className='form-img-div'>
-                        <Suspense fallback={
-                            <Skeleton className='form-img-skeleton' />
-                        }>
-                            <LazyInfoImages src={catering1} alt='catering1' />
-                        </Suspense>
-                    </div>
-                    <div className='form-img-div'>
-                        <Suspense fallback={
-                            <Skeleton className='form-img-skeleton' />
-                        }>
-                            <LazyInfoImages src={catering2} alt='catering2' />
-                        </Suspense>
-                    </div>
-                    <div className='form-img-div'>
-                        <Suspense fallback={
-                            <Skeleton className='form-img-skeleton' />
-                        }>
-                            <LazyInfoImages src={catering3} alt='catering3' />
-                        </Suspense>
-                    </div>
-                </div> */}
-                <section className="">
-                    <Swiper 
-                        className="img-container-events" 
-                        loop={true}
-                        autoplay={{
-                            delay: 2000,
-                            disableOnInteraction: false,
-                        }}
-                        speed={2000}
-                        grabCursor={true}
-                        spaceBetween={10}
-                        pagination={{
-                        clickable: true,
-                        }}
-                        breakpoints={{
-                            448: {
-                                slidesPerView: 2,
-                            },
-                            768: {
-                                slidesPerView: 3,
-                                spaceBetween: 12,
-                            },
-                        }}
-                        modules={[Autoplay]}>
+  return (
+    <>
+      <h2 className="titulo-register">COTIZÁ TU EVENTO</h2>
+      <div className="top-form">
+        <p>
+          Más de 500 eventos realizados: Corporativos, fiestas, cumpleanos y
+          casamientos.
+          <br></br>El servicio incluye a los cocineros y mozos.
+          <br></br>Trabajamos en la parrilla u horno que haya en el lugar,
+          aunque también tenemos nuestras parrillas movibles en caso de ser
+          necesarias.(*)
+        </p>
+      </div>
+      <div className="form-imges-container">
+        <Swiper
+          loop={true}
+          autoplay={{
+            delay: 2000,
+            disableOnInteraction: false,
+          }}
+          speed={2000}
+          grabCursor={true}
+          spaceBetween={10}
+          pagination={{
+            clickable: true,
+          }}
+          breakpoints={{
+            448: {
+              slidesPerView: 2,
+            },
+            768: {
+              slidesPerView: 3,
+              spaceBetween: 12,
+            },
+          }}
+          modules={[Autoplay]}
+        >
+          {DataEvents.map(({ id, image }) => {
+            return (
+              <SwiperSlide className="form-imges-container" key={id}>
+                <Suspense fallback={<Skeleton className="swiper-suspense" />}>
+                  <ImageLazy src={image} alt="" className="form-img-div" />
+                </Suspense>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      </div>
+    </>
+  );
+};
 
-                        {DataEvents.map(({id, image}) => {
-                            return (
-                                <SwiperSlide className='form-imges-container' key={id}>
-                                    <img src={image} alt="" className='form-img-div'/>
-                                </SwiperSlide>
-                            );
-                        })}
-                    </Swiper>
-                </section>
-            </div>
-        </>
-    )
-}
-
-export default EventInfo
+export default EventInfo;
