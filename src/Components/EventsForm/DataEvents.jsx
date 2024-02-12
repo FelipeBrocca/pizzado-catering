58a6c44ef4f7a088fff538d4bfcc95486492b3e5
jmacera95 <74208045+jmacera95@users.noreply.github.com>
2024-02-11 21:43:02 -0300

    Feature events slider (#3)
    * probando slider en eventos
    
    * Improve events images slider. Fix bug related to css properties
    
    * Code optimization
    
    ---------
    
    Co-authored-by: Nico <nicoabdala722@icloud.com>

---
+++ src/Components/EventsForm/DataEvents.jsx
@@ -0,0 +1,43 @@
+import ImgPizzaBandejeo from '../../public/images/IMG-20210221-WA0000.webp';
+import ImgMesaDulce from '../../public/images/PXL_20220814_041320096.webp';
+import ImgCocinero from '../../public/images/PXL_20230430_001538726.PORTRAIT.webp';
+import ImgVacio from '../../public/images/IMG_4693.webp';
+import ImgSandwichCarne from '../../public/images/IMG_4736.webp';
+import ImgHamburguesita from '../../public/images/hamburguesita.webp';
+import ImgEntradaSalmonJamon from '../../public/images/entradasSalmonJamon.webp';
+import ImgParrilla from '../../public/images/parrilla.webp';
+
+export const DataEvents = [
+  {
+    id: 1,
+    image: ImgCocinero,
+  },
+  {
+    id: 7,
+    image: ImgEntradaSalmonJamon,
+  },
+  {
+    id: 17,
+    image: ImgPizzaBandejeo,
+  },
+  {
+    id: 6,
+    image: ImgHamburguesita,
+  },
+  {
+    id: 2,
+    image: ImgMesaDulce,
+  },
+  {
+    id: 5,
+    image: ImgSandwichCarne,
+  },
+  {
+    id: 3,
+    image: ImgVacio,
+  },
+  {
+    id: 9,
+    image: ImgParrilla,
+  },
+];
--- src/Components/EventsForm/EventInfo.jsx
+++ src/Components/EventsForm/EventInfo.jsx
@@ -1,46 +1,66 @@
-import React, { lazy, Suspense } from 'react'
-import Skeleton from 'react-loading-skeleton'
-import catering1 from '../../public/images/catering1.webp'
-import catering2 from '../../public/images/catering2.webp'
-import catering3 from '../../public/images/catering3.webp'
-const LazyInfoImages = lazy(() => import('./LazyInfoImages'))
+import React, { lazy, Suspense } from 'react';
+import Skeleton from 'react-loading-skeleton';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { DataEvents } from './DataEvents';
 
-const EventInfo = () => {
-    return (
-        <>
-            <h2 className="titulo-register">COTIZÁ TU EVENTO</h2>
-            <div className='top-form'>
-                <p>
-                    Más de 500 eventos realizados: Corporativos, fiestas, cumpleanos y casamientos.
-                    <br></br>El servicio incluye a los cocineros y mozos. 
-                    <br></br>Trabajamos en la parrilla u horno que haya en el lugar, aunque también tenemos nuestras parrillas movibles en caso de ser necesarias.(*)
-                </p>
-                <div className='form-imges-container'>
-                    <div className='form-img-div'>
-                        <Suspense fallback={
-                            <Skeleton className='form-img-skeleton' />
-                        }>
-                            <LazyInfoImages src={catering1} alt='catering1' />
-                        </Suspense>
-                    </div>
-                    <div className='form-img-div'>
-                        <Suspense fallback={
-                            <Skeleton className='form-img-skeleton' />
-                        }>
-                            <LazyInfoImages src={catering2} alt='catering2' />
-                        </Suspense>
-                    </div>
-                    <div className='form-img-div'>
-                        <Suspense fallback={
-                            <Skeleton className='form-img-skeleton' />
-                        }>
-                            <LazyInfoImages src={catering3} alt='catering3' />
-                        </Suspense>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
-}
+const ImageLazy = lazy(() => import('../ImageLazy/ImageLazy'));
 
-export default EventInfo
+const EventInfo = () => (
+  <>
+    <h2 className="titulo-register">COTIZÁ TU EVENTO</h2>
+    <div className="top-form">
+      <p>
+        Más de 500 eventos realizados: Corporativos, fiestas, cumpleaños y
+        casamientos.
+        <br />
+        El servicio incluye a los cocineros y mozos.
+        <br />
+        Trabajamos en la parrilla u horno que haya en el lugar, aunque también
+        tenemos nuestras parrillas movibles en caso de ser necesarias.(*)
+      </p>
+    </div>
+    <div className="form-images-container">
+      {' '}
+      <Swiper
+        loop={true}
+        autoplay={{
+          delay: 2000,
+          disableOnInteraction: false,
+        }}
+        speed={2000}
+        grabCursor={true}
+        spaceBetween={10}
+        pagination={{
+          clickable: true,
+        }}
+        breakpoints={{
+          448: {
+            slidesPerView: 2,
+          },
+          768: {
+            slidesPerView: 3,
+            spaceBetween: 12,
+          },
+        }}
+        modules={[Autoplay]}
+      >
+        {DataEvents.map(({ id, image }) => (
+          <SwiperSlide key={id}>
+            <Suspense fallback={<Skeleton className="swiper-suspense" />}>
+              <ImageLazy
+                src={image}
+                alt="Event imagery"
+                className="form-img-div"
+              />
+            </Suspense>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  </>
+);
+
+export default EventInfo;
