0aeedb80d1dad073a559a2b894cf7092b19ebefc
jmacera95 <74208045+jmacera95@users.noreply.github.com>
2024-03-31 11:20:45 -0300

    Feature whatsapp button (#7)
    * Add whatsapp call to action button across entire web page
    
    * reduce expand and collapse transition timing

--- src/App.js
+++ src/App.js
@@ -1,40 +1,50 @@
-import React from 'react'
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './Pages/Home'
+import Home from './Pages/Home';
 import Events from './Pages/Events';
 
-import { CartProvider } from './Context/CartContext'
+import { CartProvider } from './Context/CartContext';
 import FooterLazy from './Components/Footer/LazyFooter';
 import NotFound from './Pages/NotFound';
 import { LoadingProvider } from './Context/LoadingContext';
 import PreLoader from './Components/PreLoader/PreLoader';
 import { SkeletonTheme } from 'react-loading-skeleton';
+import { FloatingWhatsApp } from 'react-floating-whatsapp';
+import logo from './public/images/logo-sin-descripcion.svg';
 
-const Header = React.lazy(() => import('./Components/Header/Header'))
-
+const Header = React.lazy(() => import('./Components/Header/Header'));
 
 function App() {
-
   return (
-    <SkeletonTheme baseColor='#313131' highlightColor='#525252'>
+    <SkeletonTheme baseColor="#313131" highlightColor="#525252">
       <LoadingProvider>
         <PreLoader />
         <BrowserRouter>
           <CartProvider>
             <Header />
             <Routes>
-              <Route path='*' element={<NotFound />} />
-              <Route exact path='/' element={<Home />} />
-              <Route exact path='/eventos' element={<Events />} />
+              <Route path="*" element={<NotFound />} />
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/eventos" element={<Events />} />
             </Routes>
             <FooterLazy />
+            <FloatingWhatsApp
+              phoneNumber="+5491140781149"
+              accountName="Pizzado Cátering"
+              avatar={logo}
+              statusMessage=""
+              chatMessage="Hola! Cómo podemos ayudarte?"
+              placeholder="Escribe un mensaje..."
+              allowEsc
+              allowClickAway
+              notification
+              notificationSound
+            />
           </CartProvider>
         </BrowserRouter>
       </LoadingProvider>
     </SkeletonTheme>
   );
 }
-
-
 
 export default App;
