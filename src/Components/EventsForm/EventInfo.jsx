7db4f547873223dd1ba62b879454a608f7af9831
jmacera95 <74208045+jmacera95@users.noreply.github.com>
2024-08-19 18:18:07 -0300

    remove swiper from event form to improve peroformance

--- src/Components/EventsForm/EventInfo.jsx
+++ src/Components/EventsForm/EventInfo.jsx
@@ -1,9 +1,5 @@
 import React, { lazy, Suspense } from 'react';
 import Skeleton from 'react-loading-skeleton';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/pagination';
 import { DataEvents } from './DataEvents';
 
 const ImageLazy = lazy(() => import('../ImageLazy/ImageLazy'));
@@ -23,42 +19,19 @@ const EventInfo = () => (
       </p>
     </div>
     <div className="form-images-container">
-      {' '}
-      <Swiper
-        loop={true}
-        autoplay={{
-          delay: 2000,
-          disableOnInteraction: false,
-        }}
-        speed={2500}
-        grabCursor={true}
-        spaceBetween={10}
-        pagination={{
-          clickable: true,
-        }}
-        breakpoints={{
-          448: {
-            slidesPerView: 2,
-          },
-          768: {
-            slidesPerView: 3,
-            spaceBetween: 12,
-          },
-        }}
-        modules={[Autoplay]}
-      >
+      <div className="image-grid">
         {DataEvents.map(({ id, image }) => (
-          <SwiperSlide key={id}>
-            <Suspense fallback={<Skeleton className="swiper-suspense" />}>
+          <div key={id} className="image-grid-item">
+            <Suspense fallback={<Skeleton className="image-suspense" />}>
               <ImageLazy
                 src={image}
                 alt="Event imagery"
                 className="form-img-div"
               />
             </Suspense>
-          </SwiperSlide>
+          </div>
         ))}
-      </Swiper>
+      </div>
     </div>
   </>
 );
