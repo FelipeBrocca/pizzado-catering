0779737bd945b05c3f0317dd93691354a1ff65b8
jmacera95 <74208045+jmacera95@users.noreply.github.com>
2024-08-19 18:20:19 -0300

    remove unused images

--- src/Components/EventsForm/DataEvents.jsx
+++ src/Components/EventsForm/DataEvents.jsx
@@ -1,12 +1,6 @@
 import ImgPizzaBandejeo from '../../public/images/optimized/IMG-20210221-WA0000.jpg';
-import ImgMesaDulce from '../../public/images/optimized/PXL_20220814_041320096.jpg';
 import ImgCocinero from '../../public/images/optimized/PXL_20230430_001538726.PORTRAIT.jpg';
-import ImgVacio from '../../public/images/optimized/IMG_4693.jpg';
-import ImgSandwichCarne from '../../public/images/optimized/IMG_4736.jpg';
-import ImgHamburguesita from '../../public/images/optimized/hamburguesita.jpg';
 import ImgEntradaSalmonJamon from '../../public/images/optimized/entradasSalmonJamon.jpg';
-import ImgParrilla from '../../public/images/optimized/parrilla.jpg';
-
 export const DataEvents = [
   {
     id: 1,
@@ -19,25 +13,5 @@ export const DataEvents = [
   {
     id: 17,
     image: ImgPizzaBandejeo,
-  },
-  {
-    id: 6,
-    image: ImgHamburguesita,
-  },
-  {
-    id: 2,
-    image: ImgMesaDulce,
-  },
-  {
-    id: 5,
-    image: ImgSandwichCarne,
-  },
-  {
-    id: 3,
-    image: ImgVacio,
-  },
-  {
-    id: 9,
-    image: ImgParrilla,
   },
 ];
