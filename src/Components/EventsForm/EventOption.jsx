68768278d442a0effab20953d5db74a5ef9a637c
jmacera95 <74208045+jmacera95@users.noreply.github.com>
2024-09-02 21:03:30 -0300

    switching to consume menu data from a backend server

--- src/Components/EventsForm/EventOption.jsx
+++ src/Components/EventsForm/EventOption.jsx
@@ -1,93 +1,58 @@
-import React from 'react'
+import React from 'react';
 
 const EventOption = ({ option, handleChange }) => {
-
-    return (
+  return (
+    <>
+      <div>
+        <label className="checkbox-style">
+          <input
+            type="checkbox"
+            id="scales"
+            name="option"
+            onClick={() => handleChange(option)}
+          />
+          <span></span>
+        </label>
+        <div className="option-container">
+          <label>{option.title}</label>
+          <p>${option.price}</p>
+        </div>
+      </div>
+      {option.food ? (
+        <ul className="menu-food-list">
+          {option.food?.map((foodOpt) => {
+            return <li key={`food - ${foodOpt}`}>{foodOpt}</li>;
+          })}
+        </ul>
+      ) : (
+        ''
+      )}
+      {option.starters && option.starters.length > 0 ? (
         <>
-            <div>
-                <label className='checkbox-style'>
-                    <input type="checkbox"
-                        id="scales"
-                        name='option'
-                        onClick={() => handleChange(option)}
-                    />
-                    <span></span>
-                </label>
-                <div className='option-container'>
-                    <label>{option.title}</label>
-                    {/* <p>${option.price}</p> */}
-                </div>
-            </div>
-            {
-                option.food ?
-                    <ul className='menu-food-list'>
-                        {
-                            option.food?.map((foodOpt) => {
-                                return (
-                                    <li key={`food - ${foodOpt}`}>
-                                        {foodOpt}
-                                    </li>
-                                )
-                            })
-                        }
-                    </ul>
-                    : ''
-            }
-            {
-                option.entradas ?
-                    <>
-                        <p>Entradas:</p>
-                        <ul className='menu-food-list'>
-                            {
-                                option.entradas?.map((foodOpt) => {
-                                    return (
-                                        <li key={`entrada - ${foodOpt}`}>
-                                            {foodOpt}
-                                        </li>
-                                    )
-                                })
-                            }
-                        </ul>
-                    </>
-                    : ''
-            }
-            {
-                option.principal ?
-                    <>
-                        <p>Principal:</p>
-                        <ul className='menu-food-list'>
-                            {
-                                option.principal?.map((foodOpt) => {
-                                    return (
-                                        <li key={`principal - ${foodOpt}`}>
-                                            {foodOpt}
-                                        </li>
-                                    )
-                                })
-                            }
-                        </ul>
-                    </>
-                    : ''
-            }
-            {
-                option.options ?
-                    <>
-                        <ul className='menu-food-list'>
-                            {
-                                option.options?.map((foodOpt) => {
-                                    return (
-                                        <li key={`postre - ${foodOpt}`}>
-                                            {foodOpt}
-                                        </li>
-                                    )
-                                })
-                            }
-                        </ul>
-                    </>
-                    : ''
-            }
+          <p>Entradas:</p>
+          <ul className="menu-food-list">
+            {option.starters?.map((foodOpt) => {
+              return <li key={`entrada - ${foodOpt}`}>{foodOpt}</li>;
+            })}
+          </ul>
         </>
-    )
-}
+      ) : (
+        ''
+      )}
+      {option.principal && option.principal.length > 0 ? (
+        <>
+          <p>Principal:</p>
+          <ul className="menu-food-list">
+            {option.principal?.map((foodOpt) => {
+              return <li key={`principal - ${foodOpt}`}>{foodOpt}</li>;
+            })}
+          </ul>
+        </>
+      ) : (
+        ''
+      )}
+    </>
+  );
+};
 
-export default EventOption 
+export default EventOption;
