584a56a7e8fef346b050a69c09233f7ef0c2af36
jmacera95 <74208045+jmacera95@users.noreply.github.com>
2024-09-06 08:55:10 -0300

    Revert "Menú editable por usuario (#12)" (#13)
    This reverts commit b2a5a85e033d495d46e1210a21ba87207f91a6ee.

--- src/Components/EventsForm/EventOption.jsx
+++ src/Components/EventsForm/EventOption.jsx
@@ -1,58 +1,93 @@
-import React from 'react';
+import React from 'react'
 
 const EventOption = ({ option, handleChange }) => {
-  return (
-    <>
-      <div>
-        <label className="checkbox-style">
-          <input
-            type="checkbox"
-            id="scales"
-            name="option"
-            onClick={() => handleChange(option)}
-          />
-          <span></span>
-        </label>
-        <div className="option-container">
-          <label>{option.title}</label>
-          {option.price ? <p>${option.price}</p> : <></>}
-        </div>
-      </div>
-      {option.food ? (
-        <ul className="menu-food-list">
-          {option.food?.map((foodOpt) => {
-            return <li key={`food - ${foodOpt}`}>{foodOpt}</li>;
-          })}
-        </ul>
-      ) : (
-        ''
-      )}
-      {option.starters && option.starters.length > 0 ? (
-        <>
-          <p>Entradas:</p>
-          <ul className="menu-food-list">
-            {option.starters?.map((foodOpt) => {
-              return <li key={`entrada - ${foodOpt}`}>{foodOpt}</li>;
-            })}
-          </ul>
-        </>
-      ) : (
-        ''
-      )}
-      {option.principal && option.principal.length > 0 ? (
-        <>
-          <p>Principal:</p>
-          <ul className="menu-food-list">
-            {option.principal?.map((foodOpt) => {
-              return <li key={`principal - ${foodOpt}`}>{foodOpt}</li>;
-            })}
-          </ul>
-        </>
-      ) : (
-        ''
-      )}
-    </>
-  );
-};
 
-export default EventOption;
+    return (
+        <>
+            <div>
+                <label className='checkbox-style'>
+                    <input type="checkbox"
+                        id="scales"
+                        name='option'
+                        onClick={() => handleChange(option)}
+                    />
+                    <span></span>
+                </label>
+                <div className='option-container'>
+                    <label>{option.title}</label>
+                    {/* <p>${option.price}</p> */}
+                </div>
+            </div>
+            {
+                option.food ?
+                    <ul className='menu-food-list'>
+                        {
+                            option.food?.map((foodOpt) => {
+                                return (
+                                    <li key={`food - ${foodOpt}`}>
+                                        {foodOpt}
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
+                    : ''
+            }
+            {
+                option.entradas ?
+                    <>
+                        <p>Entradas:</p>
+                        <ul className='menu-food-list'>
+                            {
+                                option.entradas?.map((foodOpt) => {
+                                    return (
+                                        <li key={`entrada - ${foodOpt}`}>
+                                            {foodOpt}
+                                        </li>
+                                    )
+                                })
+                            }
+                        </ul>
+                    </>
+                    : ''
+            }
+            {
+                option.principal ?
+                    <>
+                        <p>Principal:</p>
+                        <ul className='menu-food-list'>
+                            {
+                                option.principal?.map((foodOpt) => {
+                                    return (
+                                        <li key={`principal - ${foodOpt}`}>
+                                            {foodOpt}
+                                        </li>
+                                    )
+                                })
+                            }
+                        </ul>
+                    </>
+                    : ''
+            }
+            {
+                option.options ?
+                    <>
+                        <ul className='menu-food-list'>
+                            {
+                                option.options?.map((foodOpt) => {
+                                    return (
+                                        <li key={`postre - ${foodOpt}`}>
+                                            {foodOpt}
+                                        </li>
+                                    )
+                                })
+                            }
+                        </ul>
+                    </>
+                    : ''
+            }
+        </>
+    )
+}
+
+export default EventOption 
